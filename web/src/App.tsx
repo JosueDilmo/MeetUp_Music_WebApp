a54305ce501136901312ed43e35742e052a23afa
Josue <josuedilmo@gmail.com>
2023-01-28 02:03:59 +0000

    landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,20 @@
+import "./styles/main.css";
+import logoImg from "./assets/logo.svg";
+import logomMiniImg from "./assets/min_logo.svg";
+
 function App() {
-  return <h1>ONLINE</h1>;
+  return (
+    <div className="ml-6 mt-4">
+      <img src={logomMiniImg} alt="" />
+
+      <h1 className="text-8xl text-white font-bold mx-auto flex flex-col items-center mt-12 mb-2">
+        IntMus
+      </h1>
+      <h2 className="text-4xl text-white font-light mx-auto flex flex-col items-center">
+        Share your music
+      </h2>
+    </div>
+  );
 }
 
 export default App;
